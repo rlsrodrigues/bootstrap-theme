20fe75a14091808e824cb55580c3c5993d7a71f0
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2013-09-06 10:56:03 -0300

    ++ files, finish all files with default code

---
+++ archive.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+	<div class="content relative">
+
+		<?php //looping example ?>
+		<section class="content-page left">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	            
+            <?php endwhile; ?>
+        <?php endif; ?>
+        
+        </section>
+
+		<?php get_sidebar(); ?>
+
+		<section class="clear"></section>
+	</div>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,14 +1,18 @@
 <?php get_header(); ?>
-            <div class="content">
-                <div class="content-page relative">
-                    <?php if (have_posts()) : ?>
-                        <?php while (have_posts()) : the_post(); ?>
-                            <h1><?php the_title(); ?></h1>
-                            <div class="entry">
-                                <?php the_content(); ?>
-                            </div>
-                        <?php endwhile; ?>
-                    <?php endif ?>
-                </div>
-            </div>
+    <div class="content relative">
+
+        <?php //looping example ?>
+        <section class="content-page left">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
+        
+        </section>
+
+        <?php get_sidebar(); ?>
+
+        <section class="clear"></section>
+    </div>
 <?php get_footer(); ?>
