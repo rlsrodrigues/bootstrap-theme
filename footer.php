07f7c6a6e851b47284426215b3f4649b88970546
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2013-09-06 09:50:02 -0300

    ++ footer, fix html bug in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</div>
-		<div class="container-footer relative"></div>
+		<div class="container-footer relative">
 			<footer class="footer relative">
-				
+				teste
 			</footer>
 		</div>
 		<?php wp_footer(); ?>
