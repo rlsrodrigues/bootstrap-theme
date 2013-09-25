d2692c872976327bdcbfb3ce5d0c21fdd514d8fa
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2013-09-25 17:49:02 -0300

    ++ all, fix bug css, show content, create class img-block in css and correct gramatical error

--- single.php
+++ single.php
@@ -5,7 +5,11 @@
         <section class="content-page left">
 
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                
+                <h1><?php the_title(); ?></h1>
+                <section class="entry">
+                    <?php the_post_thumbnail('medium', array('class' => 'alignleft')) ?>
+                    <?php the_content(); ?>
+                </section>
             <?php endwhile; ?>
         <?php endif; ?>
         
